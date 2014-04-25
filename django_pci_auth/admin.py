0229c441788d9e492ef85de7781a144ee80b1bdf
Yotam Ofek <yotam@sookasa.com>
2014-04-24 19:30:39 -0700

    more custom user

--- django_pci_auth/admin.py
+++ django_pci_auth/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
 from django_pci_auth.models import UserProfile, PasswordLog
 
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 # https://docs.djangoproject.com/en/dev/topics/auth/\
 # #storing-additional-information-about-users
